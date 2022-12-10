a1bd165d79f3324be3f8a50bda3d57d9d647a305
Col. Schwi <VCCNStardust@gmail.com>
2022-12-10 11:49:04 +0800

    None

--- test/Apitest.py
+++ test/Apitest.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
-# @Time    : 12/5/22 5:00 PM
-# @FileName: Apitest.py
+# @Time    : 12/5/22 10:23 PM
+# @FileName: apitest.py
 # @Software: PyCharm
 # @Github    ：sudoskys
+import asyncio
 
-# 最小单元测试
-import openai
+import openai_sync
 from utils.Base import ReadConfig
 
 config = ReadConfig().parseFile("../Config/app.toml")
-openai.api_key = config.bot.OPENAI_API_KEY
-response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0,
-                                    max_tokens=20)
+click = openai_sync.Completion(api_key=config.bot.OPENAI_API_KEY)
 
+
+async def test_api():
+    return await click.create(model="text-davinci-003", prompt="Say this is a test", temperature=0,
+                              max_tokens=20, user="001")
+
+
+response = asyncio.run(test_api())
 # TEST
 print(response)
-print(type(response))
-print(response.get("choise"))
-print(response.get("choices"))
